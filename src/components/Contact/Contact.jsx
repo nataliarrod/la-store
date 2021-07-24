cec3b0da43458dd004d6dc7b6cb41932199e409c
natalia <nataliarrod@gmail.com>
2021-07-24 12:20:56 -0500

    contact component added

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,51 @@
 import React from "react";
-import useStyles from './styles';
+import useStyles from "./styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const Contact = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <h3>Contact</h3>
+      <div className={classes.textContainer}>
+        <h2>contáctame</h2>
+        <p>lineaancestral@gmail.com</p>
+      </div>
+      <form className={classes.formContainer} autoComplete="off">
+        <TextField
+          type="text"
+          name="contactName"
+          placeholder="Nombre *"
+          aria-describedby="escribir-nombre-completo"
+          required
+        />
+        <TextField
+          type="email"
+          name="contactEmail"
+          placeholder="Email *"
+          aria-describedby="escribir-email"
+          required
+        />
+        <TextField
+          type="subject"
+          name="contactSubject"
+          placeholder="Asunto"
+          aria-describedby="escribir-asunto"
+          required
+        />
+        <TextField
+          type="message"
+          name="contactMessage"
+          placeholder="Mensaje"
+          aria-describedby="escribir-mensaje"
+          required
+        />
+        <Button className={classes.buttonGrad} classvariant="contained">
+          enviar
+        </Button>
+      </form>
     </div>
   );
-}
+};
 
 export default Contact;
--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -1,14 +1,56 @@
 import { makeStyles } from '@material-ui/core';
 
-const backGround = 'white';
+const backGround = '#f6f9fc';
 
 const useStyles = makeStyles(() => ({
   root: {
     display: 'flex',
-    alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: backGround,
-    height: '100px',
+    height: '500px',
+    fontFamily: 'Poppins',
+  },
+  textContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    '& > h2': {
+      textTransform: 'capitalize',
+      fontFamily: 'Dancing Script',
+      fontSize: '50px',
+      color: '#bb5391',
+      margin: '15px 0px 5px 0px'
+    },
+    '& > p': {
+      margin: '5px 0px 15px 0px'
+    },
+  },
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    textTransform: 'capitalize',
+    alignItems: 'center',
+    width: 'auto',
+    '& .MuiInputBase-root': {
+      width: '500px',
+      height: '60px',
+    },
+  },
+  buttonGrad: {
+    background: '#bb5391',
+    borderRadius: '50px',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    textTransform: 'uppercase',
+    margin: '30px 0px',
+    fontFamily: 'Poppins',
+    '&:hover':{
+      backgroundColor: '#b63281',
+    }
   },
 }));
 
