42334a225907382908335895ca7171563fa12a3a
natalia <nataliarrod@gmail.com>
2021-07-24 15:18:54 -0500

    repair bugs on parallax,change background and change main image

--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -4,38 +4,19 @@ import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
   root: {
-    width: '100%',
-    height: '400px',
-  },
-  backgroundImage: {
-    height: '705px',
-    width: '1440px',
-    backgroundSize: 'auto',
-    backgroundRepeat: 'no-repeat',
-    backgroundImage: 'url(https://firebasestorage.googleapis.com/v0/b/la-store-5dcee.appspot.com/o/clouds.png?alt=media&token=a87613ee-17a5-4f31-b8ca-e07b611c9e17)',
-  },
-  aboutContainer: {
     display: 'flex',
-    height: '400px',
-    '& > div:firstChild': {
-      backgroundColor: 'red',
-    },
-  },
-  aboutImage: {
-    paddingLeft: '150px',
-    width: '70%',
-    minWidth: '300px'
+    justifyContent: 'center',
   },
   textAboutContainer: {
-    paddingRight: '150px',
     height: '100%',
     width: '50%',
+    paddingRight: '50px',
     '& > h2': {
       fontFamily: 'Dancing Script',
       color: '#bb3382',
       fontSize: '60px',
       textTransform: 'capitalize',
-      margin: '45px 0px',
+      margin: '35px 0px',
       textAlign: 'right'
     },
     '& > p': {
@@ -43,6 +24,7 @@ const useStyles = makeStyles(() => ({
       textAlign: 'right',
       fontSize: '16px',
       color: 'gray',
+      paddingLeft: '50px',
     },
   },
 }));
