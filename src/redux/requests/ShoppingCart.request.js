9c5bf9b1f244021a396644e3e06ea8c47a0c41a9
Natalia Rodriguez <nataliarrod@gmail.com>
2021-07-31 12:48:57 -0500

    path to get products was  updated

--- src/redux/requests/ShoppingCart.request.js
+++ src/redux/requests/ShoppingCart.request.js
@@ -1,7 +1,7 @@
 import axiosProducts from '../../utils/apiClient';
 
 export const getStoreProducts = async () => {
-  const [error, data] = await axiosProducts.get('/products')
+  const [error, data] = await axiosProducts.get('/store/products')
     .then((response) => [null, response])
     .catch((err) => [err, err]);
 
