35ff565e0f89af2459e54b6262538b492b1d1f41
natalia <nataliarrod@gmail.com>
2021-08-09 14:59:23 -0500

    theme material ui created for typography and color palette

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -4,11 +4,15 @@ export const lineaAncestralTheme = createTheme({
   palette: {
     main: {
       blue: '#f6f9fc',
-      pink: "#d91c84",
+      pink: '#d91c84',
     },
   },
   typography: {
-    title: "Dancing Script",
-    text: "Poppins",
+    title: {
+      fontFamily: 'Dancing Script',
+    },
+    text: {
+      fontFamily: 'Poppins',
+    },
   },
 });
