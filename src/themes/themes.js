6cf93938c6ff06beb7becefe286bb0168ab5c2a1
natalia <nataliarrod@gmail.com>
2021-08-09 14:52:54 -0500

    theme material ui created

---
+++ src/themes/themes.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@material-ui/core/styles";
+
+export const lineaAncestralTheme = createTheme({
+  palette: {
+    main: {
+      blue: '#f6f9fc',
+      pink: "#d91c84",
+    },
+  },
+  typography: {
+    title: "Dancing Script",
+    text: "Poppins",
+  },
+});
