c9d87ea281c0db310222e7372a565fb92ba56b41
Carol <carol_cardenas93@hotmail.com>
2021-08-10 15:32:42 -0500

    delete comments

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,27 +1,10 @@
+import { Instagram } from "@material-ui/icons";
+
 export const landingRoute = '/';
 export const shopRoute = '/tienda';
 export const orderRoute = '/pedido';
 
 export const imagebanner = 'https://firebasestorage.googleapis.com/v0/b/la-store-5dcee.appspot.com/o/bannershop.png?alt=media&token=fcd0aabf-ee64-4ce6-ba50-96e84409d49c';
-
-// export const labelsNavBar = [
-//   {
-//     name: "inicio",
-//     url: landingRoute,
-//   },
-//   {
-//     name: "tienda virtual",
-//     url: shopRoute,
-//   },
-//   {
-//     name: "sobre mí",
-//     url: `${landingRoute}#about`,
-//   },
-//   {
-//     name: "contacto",
-//     url: `${landingRoute}#contacto`,
-//   },
-// ];
 
 const URL_IMAGES = [
   {
