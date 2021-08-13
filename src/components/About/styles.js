5b06cd5708bc4fa1a175b5bddada819bea83cb34
Carol <carol_cardenas93@hotmail.com>
2021-08-13 11:02:16 -0500

    add styles to tittle

--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -2,28 +2,33 @@ import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
   root: {
-    display: 'flex',
-    justifyContent: 'center'
+    display: "flex",
+    justifyContent: "center",
   },
   textAboutContainer: {
-    height: '100%',
-    width: '50%',
-    paddingRight: '4rem',
-    '& > h2': {
-      fontFamily: 'Dancing Script',
-      color: '#d91c84',
-      fontSize: '60px',
-      textTransform: 'capitalize',
-      margin: '35px 0px',
-      textAlign: 'right'
+    height: "100%",
+    width: "50%",
+    paddingRight: "4rem",
+    "& > p": {
+      fontFamily: "Poppins",
+      textAlign: "right",
+      fontSize: "19px",
+      color: "gray",
+      paddingLeft: "50px",
+      fontWeight: "500",
     },
-    '& > p': {
-      fontFamily: 'Poppins',
-      textAlign: 'right',
-      fontSize: '19px',
-      color: 'gray',
-      paddingLeft: '50px',
-      fontWeight: '500'
+  },
+  titleAboutContainer: {
+    height: "100%",
+    width: "50%",
+    paddingRight: "4rem",
+    "& > h2": {
+      fontFamily: "Dancing Script",
+      color: "#d91c84",
+      fontSize: "60px",
+      textTransform: "capitalize",
+      margin: "35px 0px",
+      textAlign: "right",
     },
   },
 }));
