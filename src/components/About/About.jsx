3a52e5cedbf7a64239e7793507598bca5af741b2
Carol <carol_cardenas93@hotmail.com>
2021-08-13 10:54:13 -0500

    About.jsx, change all div to Grid

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,26 +4,27 @@ import aboutImage from "../../assets/img/aboutImage.png";
 import backgroundAbout from "../../assets/img/backgroundAbout.png";
 import useStyles from "./styles";
 import aboutConstants from "./constants"
+import { Grid } from "@material-ui/core";
 
 const About = () => {
   const classes = useStyles();
 
   return (
     <Parallax bgImage={backgroundAbout} strength={500}>
-      <div id="about" className={classes.root} style={{ height: 450 }}>
-        <div className={classes.aboutImage}>
+      <Grid id="about" className={classes.root} item xs={12}>
+        <Grid className={classes.aboutImage}>
           <img
             src={aboutImage}
             alt="imagen diana rodriguez"
             height="470px"
             width="530px"
           />
-        </div>
-        <div className={classes.textAboutContainer}>
+        </Grid>
+        <Grid className={classes.textAboutContainer}>
           <h2>{aboutConstants.title}</h2>
           <p data-testid="about-text"> {aboutConstants.content} </p>
-        </div>
-      </div>
+        </Grid>
+      </Grid>
     </Parallax>
   );
 };
