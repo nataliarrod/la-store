bd4a13eb9e3bce28d211f8cef9f44e8a8b01fe68
Carol Cardenas Leon <83250801+clcardenasle@users.noreply.github.com>
2021-08-17 07:25:22 -0500

    La 39 (#11) Responsive Navbar, testing navbar updated, autotype included in about, responsive about
    * constants About me
    
    * change styles testAboutContainer paddingRight to 4rem
    
    * done constants, use in About.jsx
    
    * About.jsx, change all div to Grid
    
    * change title to independent containter
    
    * add styles to tittle
    
    * change positions items, responsive xs done
    
    * rollback title container
    
    * responsive md done
    
    * responsive lg done
    
    * add marginBottom -31px to aboutImage
    
    * add function typewriter
    
    * Array function typewriter
    
    * justify content autotype professions
    
    * navbar: opacity 0.96, boxShadow 5px
    
    * add responsive to autotype
    
    * rollback components landing
    
    * add test to AutoType, change about component test, remove warnings on the console
    
    * add test to navbar links ids

--- src/components/About/About.test.js
+++ src/components/About/About.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, cleanup, screen } from '@testing-library/react';
 import About from './About';
+import aboutConstants, { typeWriterConstants } from "./constants";
 
 describe('About Component', () => {
   afterEach(cleanup);
@@ -8,8 +9,10 @@ describe('About Component', () => {
   test('render elements of the about', () => {
     render(<About/>)
 
-    expect(screen.getByText("sobre mí")).toBeInTheDocument();
-    expect(screen.getByAltText('imagen diana rodriguez')).toBeInTheDocument();
+    expect(screen.getByText(aboutConstants.title)).toBeInTheDocument();
     expect(screen.getByTestId('about-text')).toBeInTheDocument();
+    expect(screen.getByText(aboutConstants.content)).toBeInTheDocument();
+    expect(screen.getByAltText('imagen diana rodriguez')).toBeInTheDocument();
+    expect(screen.getByTestId("auto-type-wrapper")).toBeInTheDocument();
   });
 })
