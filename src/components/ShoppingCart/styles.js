aa4c7a92018b452eb6c2329abb031fe4c24563fa
Carol <carol_cardenas93@hotmail.com>
2021-08-24 16:21:53 -0500

    add margintop shoppingCart

--- src/components/ShoppingCart/styles.js
+++ src/components/ShoppingCart/styles.js
@@ -2,30 +2,31 @@ import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
   root: {
-    width: '100%',
-    height: '622px',
-    '& h2': {
-      fontFamily: 'Dancing Script',
-      color: '#d91c84',
-      fontSize: '40px',
-      textTransform: 'capitalize',
+    width: "100%",
+    height: "622px",
+    marginTop: "70px",
+    "& h2": {
+      fontFamily: "Dancing Script",
+      color: "#d91c84",
+      fontSize: "40px",
+      textTransform: "capitalize",
     },
   },
   buttonContainer: {
-    marginTop: '30px',
-    '& .MuiButtonBase-root': {
-      backgroundColor: '#d91c84',
-      color: 'white',
-      marginRight: '20px',
+    marginTop: "30px",
+    "& .MuiButtonBase-root": {
+      backgroundColor: "#d91c84",
+      color: "white",
+      marginRight: "20px",
     },
   },
   totalCount: {
-    fontFamily: 'Dancing Script',
-    color: '#d91c84',
-    textTransform: 'uppercase',
-    fontSize: '20px',
-    marginRight: '40px'
-  }
+    fontFamily: "Dancing Script",
+    color: "#d91c84",
+    textTransform: "uppercase",
+    fontSize: "20px",
+    marginRight: "40px",
+  },
 }));
 
 export default useStyles;
