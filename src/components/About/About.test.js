e0a4388dffe89097773a5df9454a704c57a272ac
Carol <carol_cardenas93@hotmail.com>
2021-08-25 17:13:03 -0500

    single quotes replaced with double quotes in About files

--- src/components/About/About.test.js
+++ src/components/About/About.test.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { render, cleanup, screen } from '@testing-library/react';
-import About from './About';
+import React from "react";
+import { render, cleanup, screen } from "@testing-library/react";
+import About from "./About";
 import aboutConstants, { typeWriterConstants } from "./constants";
 
-describe('About Component', () => {
+describe("About Component", () => {
   afterEach(cleanup);
 
-  test('render elements of the about', () => {
+  test("render elements of the about", () => {
     render(<About/>)
 
     expect(screen.getByText(aboutConstants.title)).toBeInTheDocument();
-    expect(screen.getByTestId('about-text')).toBeInTheDocument();
+    expect(screen.getByTestId("about-text")).toBeInTheDocument();
     expect(screen.getByText(aboutConstants.content)).toBeInTheDocument();
-    expect(screen.getByAltText('imagen diana rodriguez')).toBeInTheDocument();
+    expect(screen.getByAltText("imagen diana rodriguez")).toBeInTheDocument();
     expect(screen.getByTestId("auto-type-wrapper")).toBeInTheDocument();
   });
 })
