4c70aed37f663710251fe7de10ce5925e6ebdef2
Carol <carol_cardenas93@hotmail.com>
2021-08-26 15:36:10 -0500

    change single quote to double quote

--- src/components/Carousel/styles.js
+++ src/components/Carousel/styles.js
@@ -1,20 +1,20 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   mainCarouselContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    backgrounColor: '#f6f9fc',
+    display: "flex",
+    justifyContent: "center",
+    backgrounColor: "#f6f9fc",
     marginTop: "60px",
   },
   carouselComponent: {
-    width: '100%',
+    width: "100%",
   },
   imagesCarouselContainer: {
-    height: '100%',
-    '& > img': {
-      width: '100%',
-      objectFit: 'fill'
+    height: "100%",
+    "& > img": {
+      width: "100%",
+      objectFit: "fill",
     },
   },
 }));
