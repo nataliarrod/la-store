c1ed164efd085c3e6bf7099ad09228291c8e1e14
Carol <carol_cardenas93@hotmail.com>
2021-08-26 14:20:04 -0500

    Use Theme Constants in ShoppingCart Styles

--- src/components/ShoppingCart/styles.js
+++ src/components/ShoppingCart/styles.js
@@ -1,30 +1,30 @@
 import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
     height: "622px",
     marginTop: "70px",
     "& h2": {
-      fontFamily: "Dancing Script",
-      color: "#d91c84",
-      fontSize: "40px",
+      fontFamily: theme.typography.fontFamilySecondary,
+      color: theme.palette.main.pink,
+      fontSize: theme.typography.h1.fontSize,
       textTransform: "capitalize",
     },
   },
   buttonContainer: {
     marginTop: "30px",
     "& .MuiButtonBase-root": {
-      backgroundColor: "#d91c84",
-      color: "white",
+      backgroundColor: theme.palette.main.pink,
+      color: theme.palette.main.white,
       marginRight: "20px",
     },
   },
   totalCount: {
-    fontFamily: "Dancing Script",
-    color: "#d91c84",
+    fontFamily: theme.typography.fontFamilySecondary,
+    color: theme.palette.main.pink,
     textTransform: "uppercase",
-    fontSize: "20px",
+    fontSize: theme.typography.h5.fontSize,
     marginRight: "40px",
   },
 }));
