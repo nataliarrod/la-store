c036688b212cda1a291971a198ddf54f7adb3671
Carol <carol_cardenas93@hotmail.com>
2021-08-26 22:13:46 -0500

    changed single quote to double quote in constants files

--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -1,58 +1,58 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
 
-const backGround = '#f6f9fc';
+const backGround = "#f6f9fc";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: backGround,
-    height: '450px',
-    fontFamily: 'Poppins',
-    [theme.breakpoints.down('sm')]: {
-      display: '450px',
+    height: "450px",
+    fontFamily: "Poppins",
+    [theme.breakpoints.down("sm")]: {
+      display: "450px",
     },
   },
   textContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    '& > h2': {
-      textTransform: 'capitalize',
-      fontFamily: 'Dancing Script',
-      fontSize: '50px',
-      color: '#d91c84',
-      margin: '15px 0px 5px 0px'
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    "& > h2": {
+      textTransform: "capitalize",
+      fontFamily: "Dancing Script",
+      fontSize: "50px",
+      color: "#d91c84",
+      margin: "15px 0px 5px 0px",
     },
-    '& > p': {
-      margin: '5px 0px 15px 0px'
+    "& > p": {
+      margin: "5px 0px 15px 0px"
     },
   },
   formContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    textTransform: 'capitalize',
-    alignItems: 'center',
-    width: 'auto',
-    '& .MuiInputBase-root': {
-      width: '500px',
-      height: '60px'
+    display: "flex",
+    flexDirection: "column",
+    textTransform: "capitalize",
+    alignItems: "center",
+    width: "auto",
+    "& .MuiInputBase-root": {
+      width: "500px",
+      height: "60px",
     },
   },
   buttonGrad: {
-    background: '#d91c84',
-    borderRadius: '50px',
-    color: 'white',
+    background: "#d91c84",
+    borderRadius: "50px",
+    color: "white",
     height: 48,
-    padding: '0 30px',
-    textTransform: 'uppercase',
-    margin: '30px 0px',
-    fontFamily: 'Poppins',
-    '&:hover':{
-      backgroundColor: '#b63281'
+    padding: "0 30px",
+    textTransform: "uppercase",
+    margin: "30px 0px",
+    fontFamily: "Poppins",
+    "&:hover":{
+      backgroundColor: "#b63281",
     }
   },
 }));
