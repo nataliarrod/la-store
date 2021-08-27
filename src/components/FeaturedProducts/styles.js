5aa1066f57ed34ace0d7caf3e4978ba358804697
Carol <carol_cardenas93@hotmail.com>
2021-08-27 17:44:39 -0500

    use theme constants and change simple quote for double quote

--- src/components/FeaturedProducts/styles.js
+++ src/components/FeaturedProducts/styles.js
@@ -1,41 +1,40 @@
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
 
-const backGround = '#f6f9fc';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    backgroundColor: backGround,
-    height: '450px',
-    justifyContent: 'center',
-    [theme.breakpoints.down('lg')]: {
-      display: '100%',
-    }
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: theme.palette.blue,
+    height: "450px",
+    justifyContent: "center",
+    [theme.breakpoints.down("lg")]: {
+      display: "100%",
+    },
   },
   textContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    '& > h3': {
-      margin: '20px 0px 40px 20px',
-      fontFamily: 'Dancing Script',
-      color: '#d91c84',
-      fontSize: '40px',
-      textTransform: 'capitalize'
+    display: "flex",
+    justifyContent: "space-between",
+    "& > h3": {
+      margin: "20px 0px 40px 20px",
+      fontFamily: theme.typography.fontFamilySecondary,
+      color: theme.palette.pink,
+      fontSize: theme.typography.h1.fontSize,
+      textTransform: "capitalize",
     },
-    '& > a': {
-      margin: '20px 20px 0px 0px',
-      fontFamily: 'Poppins',
-      color: 'gray',
-      textTransform: 'capitalize'
+    "& > a": {
+      margin: "20px 20px 0px 0px",
+      fontFamily: theme.typography.fontFamily,
+      color: theme.palette.gray,
+      textTransform: "capitalize",
     },
   },
   productsContainer: {
-    display: 'flex',
-    width: '100%',
-    fontFamily: 'Poppins',
-    color: 'gray',
-    justifyContent: 'center'
+    display: "flex",
+    width: "100%",
+    fontFamily: theme.typography.fontFamily,
+    color: theme.palette.gray,
+    justifyContent: "center",
   },
 }));
 
